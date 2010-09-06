86a6e7719bbf97bf0c799404d562ca6f9cdbeda5
Cosimo Streppone <cosimo@cpan.org>
2010-09-06 18:33:26 +0200

    Revised code sample in the POD that had errors

--- Hunspell.pm
+++ Hunspell.pm
@@ -22,84 +22,107 @@ Text::Hunspell - Perl interface to the GNU Hunspell library
 
 =head1 SYNOPSIS
 
+    # For this example to work, you have to have
+    # the US english dictionary installed!
+
+    use strict;
+    use warnings;
+
+    use Data::Dumper ();
     use Text::Hunspell;
-    my $speller = Text::Hunspell->new("/../language.aff","/../language.dic");
+
+    # You can use relative or absolute paths.
+    my $speller = Text::Hunspell->new(
+        "/usr/share/hunspell/en_US.aff",    # Hunspell affix file
+        "/usr/share/hunspell/en_US.dic"     # Hunspell dictionary file
+    );
 
     die unless $speller;
 
+    # Check a word against the dictionary
+    my $word = 'opera';
+    print $speller->check($word)
+          ? "'$word' found in the dictionary\n"
+          : "'$word' not found in the dictionary!\n";
 
-   # Set some words
-    my $word = "lótól";
-    my $word1 = "lóotól";
-    my $misspelled = "lóo";
-
-
-    # check a word
-    print $speller->check( $word )
-          ? "$word found\n"
-          : "$word not found!\n";
-
-    # check a word
-    print $speller->check( $word )
-          ? "$word found\n"
-          : "$word not found!\n";
-    print $speller->check( $word1 )
-          ? "$word1 found\n"
-          : "$word1 not found!\n";
-
-    # lookup up words
-    my @suggestions;
-    @suggestions = $speller->suggest( $misspelled );
-    print Data::Dumper::Dumper( \@suggestions ); 
-
-    @suggestions = $speller->analyze($word);
-    print Data::Dumper::Dumper( \@suggestions ); 
- 
-    @suggestions = $speller->stem($word);
-    print Data::Dumper::Dumper( \@suggestions ); 
-
-
-    # Test here generator for morphological modification (NOM->ACC)
-    @suggestions = $speller->analyze($stem);
-    $count = @suggestions;
-
-    # modify analyze output for required class (ACC)
-    for($i = 0; $i < $count; $i++){
-       $res = @suggestions[$i];
-       $res =~ s/NOM/ACC/g;
-       @suggestions[$i] = $res;
+    # Spell check suggestions
+    my $misspelled = 'programmng';
+    my @suggestions = $speller->suggest($misspelled);
+    print "\n", "You typed '$misspelled'. Did you mean?\n";
+    for (@suggestions) {
+        print "  - $_\n";
     }
-    # generate ACC class of stem
+
+    # Analysis of a word
+    $word = 'automatic';
+    my $analysis = $speller->analyze($word);
+    print "\n", "Analysis of '$word' returns '$analysis'\n";
+
+    # Word stemming
+    $word = 'development';
+    my @stemming = $speller->stem($word);
+    print "\n", "Stemming of '$word' returns:\n";
+    for (@stemming) {
+        print "  - $_\n";
+    }
+
+    
+    #------------------------------------------
+    # ADVANCED STUFF FROM HERE
+    # NOT SURE HOW IT SHOULD WORK
+    #------------------------------------------
+
+    #
+    # Test here generator for morphological modification (NOM->ACC)
+    #
+    $word = 'developer';
+    my $stem = 'computer';
+    @suggestions = $speller->analyze($stem);
+    # Modify analyze output for required class (ACC)
+    for (@suggestions) {
+       s/NOM/ACC/g;
+    }
+    # Generate ACC class of stem
     @suggestions = $speller->generate2($stem, \@suggestions);
-    # end of generator for morphological modification (NOM->ACC) test 
+    print "Morphological modification generator...\n";
+    print Data::Dumper::Dumper(\@suggestions);
 
-    # test generator for mrphological modification, modify $stem like $word
+
+    #
+    # Test generator for morphological modification,
+    # modify $stem like $word
+    #
     @suggestions = $speller->generate($stem, $word);
+    print "Morphological modification generator...\n";
+    print Data::Dumper::Dumper(\@suggestions);
 
-    # delete speller class
+    # Deletes the underlying Hunspell C/C++ object
     $speller->delete($speller);
+
 
 =head1 DESCRIPTION
 
-This module provides a Perl interface to the Oo Hunspell library.  This module
+This module provides a Perl interface to the OO B<Hunspell> library.  This module
 is to meet the need of looking up many words, one at a time, in a single
 session, such as spell-checking a document in memory.
-
 
 The example code describes the interface on http://hunspell.sf.net
 
 
 =head1 DEPENDENCIES
 
-You MUST have installed GNU Hunspell library version 1.0  or higher on your
-system before installing this Text::Hunspell Perl module.  
+B<You MUST have installed GNU Hunspell library version 1.0 or higher>
+on your system before installing this C<Text::Hunspell> Perl module.  
 
-hunspell location is:
+Hunspell location is:
 
     http://hunspell.sf.net
 
+There have been a number of bug reports because people failed to install hunspell
+before installing this module.
 
-There have been a number of bug reports because people failed to install hunspell before installing this module.  This is an interface to the hunspell library installed on your system, not a replacement for hunspell.
+This is an interface to the hunspell library installed on your system,
+not a replacement for hunspell.
 
 You must also have one hunspell dictionary installed when running the module's
 test suite.
@@ -108,14 +131,13 @@ Also, please see the README and Changes files.  README may have specific
 information about your platform.
 
 
-
 =head1 METHODS
 
 The following methods are available:
 
 =over 4
 
-=item $speller = Text::Hunspell->new(full_path_to_affix, full_path_to_dic);
+=item $speller = Text::Hunspell->new($full_path_to_affix, $full_path_to_dic);
 
 Creates a new speller object.  New takes the parameters full_path_of_affix file and full_path_of_dic file 
 Returns C<undef> if the object could not be created, which is unlikely.
@@ -123,7 +145,8 @@ Returns C<undef> if the object could not be created, which is unlikely.
 Internally, new() creates the hunspell class.
 
 =item $speller->check($word);
-check the word. Passes back 1, if the word found, 0 otherwise.
+
+Check the word. Passes back 1, if the word found, 0 otherwise.
 
 =item $speller->suggest($misspelled_word);
 
@@ -149,32 +172,39 @@ Passes back morphologically modified stem like $word
 
 deletes the speller class.
 
+=back
 
 =head1 BUGS
 
 Probably.
-
+Yes, definitely.
 
 =head1 COPYRIGHT
 
 This library is free software; you can redistribute it
 and/or modify it under the same terms as Perl itself.
 
+=head1 AUTHORS
 
-=head1 AUTHOR
+    Eleonora, E<lt>eleonora46_at_gmx_dot_netE<gt>
 
-Eleonora eleonora46_at_gmx_dot_net
+Current maintainer is:
 
-This module is based on a perl module (Text::Aspell) written by Bill Moseley moseley@hank.org.
+    Cosimo Streppone, E<lt>cosimo@cpan.orgE<gt>
 
-Hunspell is written as myspell by Kevin B. Hendricks, hunspell is maintained by Németh László
+This module is based on a L<Text::Aspell>
+written by Bill Moseley moseley at hank dot org.
+
+Hunspell is written as myspell by Kevin B. Hendricks,
+Hunspell is maintained by Németh László.
 
 Please see:
 
     http://hunspell.sf.net
-for the dictionaries:
+
+For the dictionaries:
+
    http://lingucomponent.openoffice.org/spell_dic.html
    http://magyarispell.sf.net for Hungarian dictionary
-   
 
 =cut
